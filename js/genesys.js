2b3d43d459204b9cb4e5ef792429699580911bf4
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-04-16 16:33:03 +0200

    Update genesys.js

--- js/genesys.js
+++ js/genesys.js
@@ -23,23 +23,23 @@ function setCookie(cname,cvalue,exdays) {
 }
 
 function getCookie(cname) {
-  console.log('getCookie1 Start '); 
+  console.log('genesys.js - getCookie1 Start '); 
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
   let ca = decodedCookie.split(';');
-  console.log('getCookie2 = ',ca);
+  console.log('genesys.js - getCookie2 = ',ca);
   for(let i = 0; i < ca.length; i++) {
 	let c = ca[i];
-	console.log('getCookie3 = ',c,ca);
+	console.log('genesys.js - getCookie3 = ',c,ca);
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
     }
-	console.log('getCookie4 = ',c,ca);
+	console.log('genesys.js - getCookie4 = ',c,ca);
     if (c.indexOf(name) == 0) {
 	        return c.substring(name.length, c.length);
-			console.log('getCookie5 = ',c,ca);
+			console.log('genesys.js - getCookie5 = ',c,ca);
     }
-	console.log('getCookie6 = ',c,ca);
+	console.log('genesys.js - getCookie6 = ',c,ca);
   }
   return "";
   
@@ -57,31 +57,8 @@ function checkCookie() {
   }
 }
 
-function setupJourneyTracking() {
-    // Track all page views
-    Genesys("command", "Journey.pageview", {});
-
-    // Track forms
-    Genesys("command", "Journey.formsTrack", {
-        selector: "form",
-        captureFormDataOnAbandon: true
-    });
-
-    // Track click events on all buttons
-    Genesys("command", "Journey.trackClickEvents", {
-        clickEvents: [
-            { selector: "button.btn", eventName: "button_click" }
-        ]
-    });
-
-    // Track idle events
-    Genesys("command", "Journey.trackIdleEvents", {
-        idleEvents: [{ idleAfterSeconds: 60, eventName: "user_idle_60_seconds" }]
-    });
-}
-
 function FormTrack() {
-    console.log("Form track");
+    console.log("genesys.js - Form track");
     Genesys("command", "Journey.formsTrack", {
         selector: "form",
         captureFormDataOnAbandon: true
@@ -89,7 +66,7 @@ function FormTrack() {
 }
 
 function ButtonClickTrack() {
-    console.log("Button click track");
+    console.log("genesys.js - Button click track");
     Genesys("command", "Journey.trackClickEvents", {
         clickEvents: [
             { selector: "button.btn", eventName: "button_click" }
@@ -98,79 +75,79 @@ function ButtonClickTrack() {
 }
 
 function IdleTrack() {
-    console.log("Idle 120 seconds track");
+    console.log("genesys.js - Idle 120 seconds track");
     Genesys("command", "Journey.trackIdleEvents", {
         idleEvents: [{ idleAfterSeconds: 120, eventName: "user_idle_120_seconds" }]
     });
 }
 
 function CardSelect() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "credit_card_selection" });
 }
 
 function ClaimStage() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "claim_stage" });
 }
 
 function ClaimType() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "claim_type" });
 }
 
 function TravelClaimStage() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "travel_claim_stage" });
 }
 
 function TravelClaimType() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "travel_claim_type" });
 }
 
 function HealthCareClaimStage() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "healthcare_claim_stage" });
 }
 
 function HealthCareClaimType() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "healthcare_claim_type" });
 }
 
 function LifeClaimStage() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "life_claim_stage" });
 }
 
 function LifeClaimType() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "life_claim_type" });
 }
 
 function MotorClaimStage() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "motor_claim_stage" });
 }
 
 function MotorClaimType() {
-console.log("Dropdown clicked");
+console.log("genesys.js - Dropdown clicked");
 Genesys("command", "Journey.record", { eventName: "motor_claim_type" });
 }
 
 function setupJourneySubscriptions() {
     // Subscribe to readiness
     Genesys("subscribe", "Journey.ready", function() {
-      console.log("GPE Journey plugin is ready.")
+      console.log("genesys.js - GPE Journey plugin is ready.")
     });
 
     // Subscribe to open actions
     Genesys("subscribe", "Journey.qualifiedOpenAction", function(event) {
-        console.log("Received GPE qualified open action event:", event);
+        console.log("genesys.js - Received GPE qualified open action event:", event);
 
         if (event.data.openActionProperties.openActionName.startsWith("GPE and SF - ")) {
-            console.log("Triggering Salesforce Web Messaging...");
+            console.log("genesys.js - Triggering Salesforce Web Messaging...");
             launchSalesforceChat();
         }
     });
@@ -178,9 +155,9 @@ function setupJourneySubscriptions() {
 
 
 var username='';
-console.log('Email address is ',this.username);
+console.log('genesys.js - Email address is ',this.username);
 username=getCookie("username");
-console.log('Email address is ',this.username);
+console.log('genesys.js - Email address is ',this.username);
 Genesys("command", "Database.set", {
     messaging: {
         customAttributes: {
