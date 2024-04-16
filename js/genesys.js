3e730482224d9d356d201c4aee1748d8e02e48f8
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-04-16 15:17:34 +0200

    Update genesys.js

--- js/genesys.js
+++ js/genesys.js
@@ -2,18 +2,18 @@
 * Genesys Digtal: GPE
 */
 
- (function (g, e, n, es, ys) {
-    g['_genesysJs'] = e;
-    g[e] = g[e] || function () {
-      (g[e].q = g[e].q || []).push(arguments)
-    };
-    g[e].t = 1 * new Date();
-    g[e].c = es;
-    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
-  })(window, 'Genesys', 'https://apps.mypurecloud.de/genesys-bootstrap/genesys.min.js', {
-    environment: 'prod-euc1',
-    deploymentId: 'db32d60a-f67c-4954-986e-caf4f00517a9'
-  });
+(function (g, e, n, es, ys) {
+  g['_genesysJs'] = e;
+  g[e] = g[e] || function () {
+    (g[e].q = g[e].q || []).push(arguments)
+  };
+  g[e].t = 1 * new Date();
+  g[e].c = es;
+  ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+})(window, 'Genesys', 'https://apps.mypurecloud.de/genesys-bootstrap/genesys.min.js', {
+  environment: 'prod-euc1',
+  deploymentId: '822d3028-017c-4818-a9e5-d806df4e6c88'
+});
 
 function setCookie(cname,cvalue,exdays) {
   const d = new Date();
