12e93d2d831cf78442b2d908141ff4af43056838
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-05-06 15:42:28 +0200

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -5,6 +5,7 @@ module.exports = (req, res) => {
         GCMessagingDeplId: process.env.GC_MESSAGING_DEPLOYMENT_ID,
         SFOrgId: process.env.SF_ORG_ID,
         SFWMName: process.env.SF_WM_NAME,
-        SFWMURL: process.env.SF_WM_URL
+        SFWMURL: process.env.SF_WM_URL,
+        customerEmail: process.env.CUSTOMER_EMAIL
     });
 };
