e4bf3cde956888d30dd8804b45bbc1b2836720f6
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-05-06 16:01:35 +0200

    Update SFsnippet.js

--- js/SFsnippet.js
+++ js/SFsnippet.js
@@ -1,16 +1,15 @@
-// The Salesforce web messaging snippet
 function initEmbeddedMessaging() {
     console.log('SFsnippet.js - Initializing Embedded Messaging...');
     try {
         embeddedservice_bootstrap.settings.language = 'en-US';
-        embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true; // Hide chat button initially
+        embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true; 
 
         embeddedservice_bootstrap.init(
-            '00Dav0000006ihV',
-            'Salesforce_web_messaging', 
-            'https://cxcloudsaemea-dev-ed.develop.my.site.com/ESWSalesforcewebmessagi1712838660793',
+            window.SFOrgId,
+            window.SFWMName,
+            window.SFWMURL,
             {
-                scrt2URL: 'https://cxcloudsaemea-dev-ed.develop.my.salesforce-scrt.com'
+                scrt2URL: window.SFWMURL.replace('develop.my.site.com', 'develop.my.salesforce-scrt.com') 
             }
         );
         console.log('SFsnippet.js - Embedded Messaging initialized successfully.');
@@ -32,22 +31,18 @@ function launchSalesforceChat() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    console.log('SFsnippet.js - DOM fully loaded and parsed.');
-    var bootstrapScript = document.createElement('script');
-    bootstrapScript.type = 'text/javascript';
-    bootstrapScript.src = 'https://cxcloudsaemea-dev-ed.develop.my.site.com/ESWSalesforcewebmessagi1712838660793/assets/js/bootstrap.min.js';
-    bootstrapScript.onload = function() {
-        console.log('SFsnippet.js - Bootstrap script loaded.');
-        initEmbeddedMessaging();
-    };
-    bootstrapScript.onerror = function() {
-        console.error('SFsnippet.js - Failed to load the Bootstrap script.');
-    };
-    document.body.appendChild(bootstrapScript);
-});
+var bootstrapScript = document.createElement('script');
+bootstrapScript.type = 'text/javascript';
+bootstrapScript.src = window.SFWMURL + '/assets/js/bootstrap.min.js';
+bootstrapScript.onload = function() {
+    console.log('SFsnippet.js - Bootstrap script loaded.');
+    initEmbeddedMessaging();
+};
+bootstrapScript.onerror = function() {
+    console.error('SFsnippet.js - Failed to load the Bootstrap script.');
+};
+document.body.appendChild(bootstrapScript);
 
-// Listen for the onEmbeddedMessagingReady event
 window.addEventListener("onEmbeddedMessagingReady", () => {
     console.log("SFsnippet.js - Received the onEmbeddedMessagingReady event...");
 });
