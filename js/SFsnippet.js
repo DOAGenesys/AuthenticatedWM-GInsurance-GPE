20505150e527222a287ccc4579980351f29007c3
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-05-06 16:06:21 +0200

    Update SFsnippet.js

--- js/SFsnippet.js
+++ js/SFsnippet.js
@@ -2,19 +2,24 @@ function initEmbeddedMessaging() {
     console.log('SFsnippet.js - Initializing Embedded Messaging...');
     try {
         embeddedservice_bootstrap.settings.language = 'en-US';
-        embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true; 
+        embeddedservice_bootstrap.settings.hideChatButtonOnLoad = true;
+
+        var calculatedScrt2URL = window.SFWMURL.replace('develop.my.site.com', 'develop.my.salesforce-scrt.com');
+
+        console.log("SFsnippet.js - Salesforce Org ID:", window.SFOrgId);
+        console.log("SFsnippet.js - Salesforce Web Messaging Name:", window.SFWMName);
+        console.log("SFsnippet.js - Salesforce Web Messaging URL:", window.SFWMURL);
+        console.log("SFsnippet.js - Calculated scrt2URL:", calculatedScrt2URL);
 
         embeddedservice_bootstrap.init(
             window.SFOrgId,
             window.SFWMName,
             window.SFWMURL,
-            {
-                scrt2URL: window.SFWMURL.replace('develop.my.site.com', 'develop.my.salesforce-scrt.com') 
-            }
+            { scrt2URL: calculatedScrt2URL }
         );
         console.log('SFsnippet.js - Embedded Messaging initialized successfully.');
     } catch (err) {
-        console.error('SFsnippet.js - Error loading Embedded Messaging: ', err);
+        console.error('SFsnippet.js - Error loading Embedded Messaging:', err);
     }
 }
 
@@ -23,7 +28,6 @@ function launchSalesforceChat() {
         console.error("Salesforce Embedded Service is not yet initialized.");
         return;
     }
-
     embeddedservice_bootstrap.utilAPI.launchChat().then(() => {
         console.log("Salesforce chat launched successfully.");
     }).catch(error => {
@@ -34,6 +38,9 @@ function launchSalesforceChat() {
 var bootstrapScript = document.createElement('script');
 bootstrapScript.type = 'text/javascript';
 bootstrapScript.src = window.SFWMURL + '/assets/js/bootstrap.min.js';
+
+console.log("SFsnippet.js - Bootstrap script source URL:", bootstrapScript.src);
+
 bootstrapScript.onload = function() {
     console.log('SFsnippet.js - Bootstrap script loaded.');
     initEmbeddedMessaging();
@@ -43,6 +50,7 @@ bootstrapScript.onerror = function() {
 };
 document.body.appendChild(bootstrapScript);
 
+// Listen for the onEmbeddedMessagingReady event
 window.addEventListener("onEmbeddedMessagingReady", () => {
     console.log("SFsnippet.js - Received the onEmbeddedMessagingReady event...");
 });
