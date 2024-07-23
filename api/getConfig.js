aefed1515e865b288eff3f4cea16684c723e36c1
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-07-23 18:38:27 +0200

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -4,7 +4,6 @@ module.exports = (req, res) => {
         GCEnvironment: process.env.GC_ENVIRONMENT,
         GCMessagingDeplId: process.env.GC_MESSAGING_DEPLOYMENT_ID,
         GoogleCloudClientId: process.env.GOOGLE_CLIENT_ID,
-        GoogleCloudClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        customerEmail: process.env.CUSTOMER_EMAIL
+        GoogleCloudClientSecret: process.env.GOOGLE_CLIENT_SECRET
     });
 };
