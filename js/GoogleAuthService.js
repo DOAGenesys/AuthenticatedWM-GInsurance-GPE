778931fd60fc13e1a5f7eb64c7a327859af700ae
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-07-24 17:25:19 +0200

    Update GoogleAuthService.js

--- js/GoogleAuthService.js
+++ js/GoogleAuthService.js
@@ -14,7 +14,6 @@ export async function signIn() {
     window.location.href = `${GOOGLE_AUTH_URL}?${params.toString()}`;
 }
 
-
 export async function handleAuthCallback() {
     await window.initializationPromise;  
     const urlParams = new URLSearchParams(window.location.search);
