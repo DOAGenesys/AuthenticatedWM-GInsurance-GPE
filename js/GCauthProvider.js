57b9cf5f69639a9bfaf8edf8f7c7a388dec04cf8
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2024-07-24 18:39:42 +0200

    Update GCauthProvider.js

--- js/GCauthProvider.js
+++ js/GCauthProvider.js
@@ -9,28 +9,20 @@ if (window.initializationPromise) {
     console.error("Initialization promise not found. Make sure init.js is loaded first.");
 }
 
-
 function initializeAuthProvider() {
     Genesys('registerPlugin', 'AuthProvider', (AuthProvider) => {
         AuthProvider.registerCommand('getAuthCode', (e) => {
             const authCode = localStorage.getItem('authCode');
             console.log('AuthProvider_snippet.js - Retrieved authCode:', authCode);
             
-            e.resolve({
-                authCode: authCode,
-                redirectUri: window.location.origin + '/index.html',
-            });
+            if (authCode) {
+                e.resolve({
+                    authCode: authCode,
+                    redirectUri: window.location.origin + '/index.html',
+                });
+            } else {
+                e.reject(new Error('Auth code not found in localStorage'));
+            }
         });
-
-        AuthProvider.subscribe('Auth.loggedOut', () => {
-            console.log('AuthProvider_snippet.js - Logged out event received.');
-        });
-
-        AuthProvider.subscribe('Auth.authError', (error) => {
-            console.error('AuthProvider_snippet.js - Auth error event received:', error);
-        });
-
-        AuthProvider.ready();
-        console.log("AuthProvider_snippet.js - AuthProvider plugin ready.");
     });
 }
